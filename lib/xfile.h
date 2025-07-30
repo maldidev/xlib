eac0071e9fd68f1c8b90e6c91caf6407f3c2e95c
maldidev <m96023646@gmail.com>
2025-07-30 12:08:37 +0300

    Update xfile.h

--- lib/xfile.h
+++ lib/xfile.h
@@ -1,3 +1,4 @@
+/*old code
 #ifndef XFILE_H
 #define XFILE_H
 #include <xlang.h>
@@ -10,6 +11,35 @@ funct xfile_read(const i8_t *path, ui8_t **buf, S_t *size) {
     fseek(f, 0, SEEK_SET);
     *buf = xmalloc(*size);
     fread(*buf, 1, *size, f);
+    fclose(f);
+    return 0;
+}
+#endif
+*/
+#ifndef XFILE_H
+#define XFILE_H
+#include <xlang.h>
+#include "xmem.h"
+
+funct xfile_read(const i8_t* path, ui8_t** buf, S_t* size) {
+    FILE* f = fopen(path, "rb");
+    if(!f) return 1;
+    
+    fseek(f, 0, SEEK_END);
+    *size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    
+    if(xmalloc_wrap(*size, (void**)buf)) {
+        fclose(f);
+        return 1;
+    }
+    
+    if(fread(*buf, 1, *size, f) != *size) {
+        xfree(*buf);
+        fclose(f);
+        return 1;
+    }
+    
     fclose(f);
     return 0;
 }
